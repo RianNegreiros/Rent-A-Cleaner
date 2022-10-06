31d028f3de5382f5fb058836a23af22669365bb6
Rian Negreiros Dos Santos <riannegreiros@gmail.com>
2022-10-06 11:14:10 -0300

    add dashboard and accounts routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root 'static_pages#root'
   post '/webhooks/:source', to: 'webhooks#create'
+
+  resource :dashboard
+  resource :accounts
 end
