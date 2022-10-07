bdd214ed5f0cd36aa6ec743ab07f08e838468286
Rian Negreiros Dos Santos <riannegreiros@gmail.com>
2022-10-07 18:31:35 -0300

    add product form controller

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -4,5 +4,5 @@
 
 import { application } from "./application"
 
-import HelloController from "./hello_controller"
-application.register("hello", HelloController)
+import ProductFormController from "./product_form_controller"
+application.register("product-form", ProductFormController)
