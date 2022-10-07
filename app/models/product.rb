acaad9a791c7471e7fe53672be5cfeca5180ea84
Rian Negreiros Dos Santos <riannegreiros@gmail.com>
2022-10-07 18:30:14 -0300

    product_data

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,9 @@ class Product < ApplicationRecord
   validates :name, presence: true
   validates :description, presence: true
   belongs_to :user
+
+  def product_data
+    return if data.blank?
+    Stripe::Product.construct_from(JSON.parse(data))
+  end
 end
