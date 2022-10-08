5802e6b42c1bf9d9836a9aa3e6f01132403d784c
Rian Negreiros Dos Santos <riannegreiros@gmail.com>
2022-10-08 16:24:24 -0300

    add photo to product model

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,6 +3,11 @@ class Product < ApplicationRecord
   validates :description, presence: true
   belongs_to :user
 
+  has_one_attached :photo do |photo|
+    photo.variant :thumb, resize_to_limit: [100, 100]
+    photo.variant :medium, resize_to_limit: [400, 400]
+  end
+
   def product_data
     return if data.blank?
     Stripe::Product.construct_from(JSON.parse(data))
