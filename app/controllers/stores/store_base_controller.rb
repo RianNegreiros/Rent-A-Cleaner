57f02a4931b77573f0bd869692f9c6bfc95d21ba
Rian Negreiros Dos Santos <riannegreiros@gmail.com>
2022-10-09 14:01:41 -0300

    add stores controllers

---
+++ app/controllers/stores/store_base_controller.rb
@@ -0,0 +1,8 @@
+class Stores::StoreBaseController < ApplicationController
+  layout 'stores'
+  before_action :set_store
+
+  def set_store
+    @store ||= Store.find_by_request(request)
+  end
+end
