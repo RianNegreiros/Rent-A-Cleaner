4476084121dbb6216b151389b769655d7995f0c2
Rian Negreiros Dos Santos <riannegreiros@gmail.com>
2022-10-09 14:01:20 -0300

    add route for store and subdomains

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,22 @@
+require 'constraints/domain_constraint'
+
 Rails.application.routes.draw do
   devise_for :users
   root 'static_pages#root'
   post '/webhooks/:source', to: 'webhooks#create'
 
+  constraints DomainConstraint do
+      scope module: :stores do
+        resources :products
+        root to: 'products#index', as: 'store_root'
+      end
+  end
+
   resource :dashboard
+  resource :checkout
+  resource :store
+  
   resources :accounts
   resources :payouts, only: [:create]
   resources :products
-  resource :checkout
 end
