a9316ffe6d20f2856a7bda97c7cf33f6a90a513a
Rian Negreiros Dos Santos <riannegreiros@gmail.com>
2022-10-10 09:53:23 -0300

    add tailwind plugin

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   plugins: [
-    require('@tailwindcss/forms')
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/aspect-ratio')
   ],
   content: [
     './app/views/**/*.html.erb',
