c6478a5cb26915858dd192ba573639555618d672
Rian Negreiros Dos Santos <riannegreiros@gmail.com>
2022-10-10 17:19:55 -0300

    add store to product

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,6 +2,8 @@ class Product < ApplicationRecord
   validates :name, presence: true
   validates :description, presence: true
   belongs_to :user
+  has_one :store, through: :user
+
 
   has_one_attached :photo do |photo|
     photo.variant :thumb, resize_to_limit: [100, 100]
