de9d11a9074734462ac6c1f57e95a604031fbe0f
Rian Negreiros Dos Santos <riannegreiros@gmail.com>
2022-10-11 12:02:16 -0300

    fix products routing

--- app/controllers/stores/store_base_controller.rb
+++ app/controllers/stores/store_base_controller.rb
@@ -1,8 +1,10 @@
-class Stores::StoreBaseController < ApplicationController
-  layout 'stores'
-  before_action :set_store
+module Stores
+  class StoreBaseController < ApplicationController
+    layout 'stores'
+    before_action :set_store
 
-  def set_store
-    @store ||= Store.find_by_request(request)
+    def set_store
+      @store ||= Store.find_by_request(request)
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,26 +2,28 @@ require 'constraints/domain_constraint'
 
 Rails.application.routes.draw do
   devise_for :users
-  root 'static_pages#root'
   post '/webhooks/:source', to: 'webhooks#create'
 
   constraints DomainConstraint do
-      scope module: :stores do
-        resources :products
-        root to: 'products#index', as: 'store_root'
-        resource :checkout, as: 'store_checkout'
-      end
+    scope module: :stores do
+      resources :products
+      root to: 'products#index', as: 'store_root'
+      resource :checkout, as: 'store_checkout'
+    end
   end
 
+  root 'static_pages#root'
+
   resource :dashboard
-  resource :checkout
-  resource :store
-  
+  resources :accounts
+  resources :payouts, only: [:create]
+
   resources :products do
     resources :attachments, shallow: true
   end
-  resources :accounts
-  resources :payouts, only: [:create]
-  resources :products
+
+  resource :store
+
+  resource :checkout
   resources :customers
 end
