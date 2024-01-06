2f059077c4676375c233fa6b22797fe221d42f2a
Rian Negreiros Dos Santos <riannegreiros@gmail.com>
2024-01-06 10:30:58 -0300

    chore: add templates

--- src/main/java/com/github/riannegreiros/ExpressCleaning/ExpressCleaningApplication.java
+++ src/main/java/com/github/riannegreiros/ExpressCleaning/ExpressCleaningApplication.java
@@ -1,4 +1,4 @@
-package com.github.riannegreiros.expresscleaning.ExpressCleaning;
+package com.github.riannegreiros.ExpressCleaning;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
