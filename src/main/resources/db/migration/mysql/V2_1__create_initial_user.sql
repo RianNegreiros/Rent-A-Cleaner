4897f89b9a0b4a2a45360851f089140a42b04e33
Rian Negreiros Dos Santos <riannegreiros@gmail.com>
2024-01-09 14:50:11 -0300

    fix: database seed and migration

--- src/main/resources/db/migration/mysql/V2_1__create_initial_user.sql
+++ src/main/resources/db/migration/mysql/V2_1__create_initial_user.sql
@@ -1,1 +1,1 @@
-INSERT INTO `user` VALUES (1,'admin@mail.com','User ADMIN','$2a$12$9H9wn28vWc0TowTGVbXsbe7DyLaMVhHSyFmJc7VoFsp1AeZIUmoyK','ADMIN');
+INSERT INTO `user` VALUES (1,'admin@mail.com','User ADMIN','$2a$12$vIKu2B8h3p479jsCqUaJt.mKFyKnrHa.IsMLGbHex3PxVKPFMmT22','ADMIN');
