db65c045adcdd43a9a31d9848e8e45c642c021dd
Rian Negreiros Dos Santos <riannegreiros@gmail.com>
2024-01-15 18:41:30 -0300

    fix: add column name

--- src/main/java/com/github/riannegreiros/ExpressCleaning/core/models/User.java
+++ src/main/java/com/github/riannegreiros/ExpressCleaning/core/models/User.java
@@ -13,7 +13,7 @@ public class User {
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
 
-  @Column(nullable = false)
+  @Column(name = "full_name", nullable = false)
   private String fullName;
 
   @Column(nullable = false, unique = true)
--- src/main/resources/db/migration/mysql/V2__create_table_user.sql
+++ src/main/resources/db/migration/mysql/V2__create_table_user.sql
@@ -1,7 +1,7 @@
 CREATE TABLE `user` (
   `id` bigint NOT NULL AUTO_INCREMENT,
   `email` varchar(255) NOT NULL,
-  `fullName` varchar(255) NOT NULL,
+  `full_name` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL,
   `user_type` enum('ADMIN','CLIENT','HOUSEKEEPER') NOT NULL,
   PRIMARY KEY (`id`),
