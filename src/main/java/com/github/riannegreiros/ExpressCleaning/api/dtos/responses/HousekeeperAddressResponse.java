e1a61ff43d032397d3880611f7d9bb41db818d89
Rian Negreiros Dos Santos <riannegreiros@gmail.com>
2024-01-16 13:10:07 -0300

    feat: add cleaner address

---
+++ src/main/java/com/github/riannegreiros/ExpressCleaning/api/dtos/responses/HousekeeperAddressResponse.java
@@ -0,0 +1,16 @@
+package com.github.riannegreiros.ExpressCleaning.api.dtos.responses;
+
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
+public class HousekeeperAddressResponse {
+    private Long id;
+    private String street;
+    private String number;
+    private String neighborhood;
+    private String complement;
+    private String zipCode;
+    private String city;
+    private String state;
+}
