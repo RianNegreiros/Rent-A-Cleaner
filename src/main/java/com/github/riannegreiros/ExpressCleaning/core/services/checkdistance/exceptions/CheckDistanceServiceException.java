91a6ba98066ba1dd85083887cc7105ae3d1a5fd2
Rian Negreiros Dos Santos <riannegreiros@gmail.com>
2024-01-17 08:44:23 -0300

    feat: implement check distance service with Google matrix

---
+++ src/main/java/com/github/riannegreiros/ExpressCleaning/core/services/checkdistance/exceptions/CheckDistanceServiceException.java
@@ -0,0 +1,11 @@
+package com.github.riannegreiros.ExpressCleaning.core.services.checkdistance.exceptions;
+
+public class CheckDistanceServiceException extends RuntimeException {
+
+    public CheckDistanceServiceException() {}
+
+    public CheckDistanceServiceException(String message) {
+        super(message);
+    }
+
+}
