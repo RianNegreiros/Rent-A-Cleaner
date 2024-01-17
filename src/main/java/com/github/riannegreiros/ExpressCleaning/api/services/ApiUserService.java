8706616dcb51a16b0faea6c7ba10ce769e8320d9
Rian Negreiros Dos Santos <riannegreiros@gmail.com>
2024-01-17 13:32:12 -0300

    chore: update HATEOAS

--- src/main/java/com/github/riannegreiros/ExpressCleaning/api/services/ApiUserService.java
+++ src/main/java/com/github/riannegreiros/ExpressCleaning/api/services/ApiUserService.java
@@ -1,11 +1,14 @@
 package com.github.riannegreiros.ExpressCleaning.api.services;
 
+import com.github.riannegreiros.ExpressCleaning.api.dtos.requests.UpdateUserRequest;
 import com.github.riannegreiros.ExpressCleaning.api.dtos.requests.UserRequest;
 import com.github.riannegreiros.ExpressCleaning.api.dtos.responses.MessageResponse;
 import com.github.riannegreiros.ExpressCleaning.api.dtos.responses.TokenResponse;
 import com.github.riannegreiros.ExpressCleaning.api.dtos.responses.UserRegisterResponse;
 import com.github.riannegreiros.ExpressCleaning.api.dtos.responses.UserResponse;
 import com.github.riannegreiros.ExpressCleaning.api.mappers.ApiUserMapper;
+import com.github.riannegreiros.ExpressCleaning.core.exceptions.WrongPasswordException;
+import com.github.riannegreiros.ExpressCleaning.core.models.User;
 import com.github.riannegreiros.ExpressCleaning.core.publishers.NewUserPublisher;
 import com.github.riannegreiros.ExpressCleaning.core.utils.SecurityUtils;
 import com.github.riannegreiros.ExpressCleaning.core.exceptions.PasswordsDoNotMatchException;
@@ -18,6 +21,8 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.validation.FieldError;
 import org.springframework.web.multipart.MultipartFile;
+
+import static org.apache.commons.lang3.ObjectUtils.firstNonNull;
 
 @Service
 public class ApiUserService {
@@ -68,7 +73,7 @@ public class ApiUserService {
         newUserPublisher.publish(registeredUser);
 
         var response = mapper.toRegisterResponse(registeredUser);
-        var tokenResponse = gerarTokenResponse(response);
+        var tokenResponse = generateTokenResponse(response);
         response.setToken(tokenResponse);
         return response;
     }
@@ -81,7 +86,67 @@ public class ApiUserService {
         return new MessageResponse("Photo saved successfully!");
     }
 
-    private TokenResponse gerarTokenResponse(UserRegisterResponse response) {
+    public MessageResponse update(UpdateUserRequest request) {
+        var userLogged = securityUtils.getLoggedUser();
+
+        updateLoggedUserInformation(request, userLogged);
+
+        validator.validate(userLogged);
+
+        changePassword(request, userLogged);
+
+        repository.save(userLogged);
+
+        return new MessageResponse("User successfully updated");
+    }
+
+    private void changePassword(UpdateUserRequest request, User userLogged) {
+        var hasPasswords = request.getPassword() != null
+                && request.getNewPassword() != null
+                && request.getPasswordConfirmation() != null;
+
+        if (hasPasswords) {
+            verifyPassword(request, userLogged);
+            validatePasswordConfirmation(request);
+            var newPassword = request.getNewPassword();
+            var newPasswordHash = passwordEncoder.encode(newPassword);
+            userLogged.setPassword(newPasswordHash);
+        }
+    }
+
+    private void verifyPassword(UpdateUserRequest request, User userLogged) {
+        var passwordRequest = request.getPassword();
+        var passwordDB = userLogged.getPassword();
+
+        if (!passwordEncoder.matches(passwordRequest, passwordDB)) {
+            var message = "The password entered is incorrect";
+            var fieldError = new FieldError(request.getClass().getName(), "password", passwordRequest, false, null, null, message);
+            throw new WrongPasswordException(message, fieldError);
+        }
+    }
+
+    private void updateLoggedUserInformation(UpdateUserRequest request, User userLogged) {
+        userLogged.setFullName(
+                firstNonNull(request.getFullName(), userLogged.getFullName())
+        );
+        userLogged.setEmail(
+                firstNonNull(request.getEmail(), userLogged.getEmail())
+        );
+        userLogged.setCpf(
+                firstNonNull(request.getCpf(), userLogged.getCpf())
+        );
+        userLogged.setBirth(
+                firstNonNull(request.getBirth(), userLogged.getBirth())
+        );
+        userLogged.setTelephone(
+                firstNonNull(request.getTelephone(), userLogged.getTelephone())
+        );
+        userLogged.setPixKey(
+                firstNonNull(request.getPixKey(), userLogged.getPixKey())
+        );
+    }
+
+    private TokenResponse generateTokenResponse(UserRegisterResponse response) {
         var token = tokenService.generateAccessToken(response.getEmail());
         var refresh = tokenService.generateRefreshToken(response.getEmail());
         return new TokenResponse(token, refresh);
@@ -97,6 +162,18 @@ public class ApiUserService {
 
     private void validatePasswordConfirmation(UserRequest request) {
         var password = request.getPassword();
+        var passwordConfirmation = request.getPasswordConfirmation();
+
+        if (!password.equals(passwordConfirmation)) {
+            var message = "the two password fields don't match";
+            var fieldError = new FieldError(request.getClass().getName(), "passwordConfirmation", request.getPasswordConfirmation(), false, null, null, message);
+
+            throw new PasswordsDoNotMatchException(message, fieldError);
+        }
+    }
+
+    private void validatePasswordConfirmation(UpdateUserRequest request) {
+        var password = request.getNewPassword();
         var passwordConfirmation = request.getPasswordConfirmation();
 
         if (!password.equals(passwordConfirmation)) {
